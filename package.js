5bbd2bf8251cc1f254490768b01bf47f48611bbb
schnie <greg@differential.com>
2015-06-03 13:12:05 -0400

    switch gh org

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'astronomer',
   version: '0.0.1',
   summary: 'Easily push analytics events to astronomer.',
-  git: 'https://github.com/usercycle/meteor-astronomer',
+  git: 'https://github.com/astronomerio/meteor-astronomer',
   documentation: 'README.md'
 });
 
