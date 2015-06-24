3e7a50d407c1daf5adb45327da74547d21d46f23
schnie <greg@differential.com>
2015-06-24 09:08:29 -0400

    SAVEPOINT

--- package.js
+++ package.js
@@ -1,20 +1,24 @@
 Package.describe({
-  name: 'astronomerio:core',
-  version: '0.0.2',
-  summary: 'Easily push analytics events to astronomer.',
-  git: 'https://github.com/astronomerio/meteor-astronomer',
-  documentation: 'README.md'
+  name: "astronomerio:core",
+  version: "0.0.2",
+  summary: "Easily push analytics events to astronomer.",
+  git: "https://github.com/astronomerio/meteor-astronomer",
+  documentation: "README.md"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('underscore');
-  api.addFiles('lib/analytics.min.js', 'client');
-  api.addFiles('dist/astronomer.js', 'client');
+  api.versionsFrom("1.0");
+  api.use([
+    "underscore",
+    "ddp",
+    "grigio:babel"
+  ]);
+  api.addFiles("lib/analytics.min.js", "client");
+  api.addFiles("src/astronomer.es6.js", "client");
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('astronomerio:core');
-  api.addFiles('astronomer-tests.js');
+  api.use("tinytest");
+  api.use("astronomerio:core");
+  api.addFiles("astronomer-tests.js");
 });
---
+++ src/astronomer.es6.js
@@ -0,0 +1,139 @@
+/* global FlowRouter, Router, analytics */
+
+"use strict";
+
+/**
+ * Setup an autorun, to identify a user whenever Meteor.userId changes.
+ */
+function setupIdentify() {
+    Tracker.autorun(() => {
+        let userId = Meteor.userId();
+        if (userId) {
+            analytics.identify(userId);
+        }
+    });
+}
+
+/**
+ * Take a source object and extend it with session keys.
+ * @returns {Object} The new properties object.
+ */
+function createProperties(obj={}) {
+    return _.extend(obj, { session: Session.keys });
+}
+
+/**
+ * Detect the router and hook in to run analytics.page.
+ */
+function setupRouteTracking() {
+
+    function page(pageName, properties={}) {
+        analytics.page(pageName, createProperties(properties));
+    }
+
+    if (typeof Router !== "undefined") {
+        /** Setup Iron Router */
+        Router.onRun(function() {
+
+            /** Build properties to pass along with page */
+            let routeParams = {};
+            let keys = _.keys(this.params);
+            _.each(keys, (key) => { routeParams[key] = this.params[key]; });
+
+            /** Get the page name */
+            let pageName = this.route.getName() || "Home";
+
+            /** Send the page view with properties */
+            page(pageName, { routeParams });
+            this.next();
+        });
+    } else if (typeof FlowRouter !== "undefined") {
+        /** Setup Flow Router */
+        FlowRouter.middleware(function(path, next) {
+
+            /** Get the page name */
+            let pageName = path !== "/" ? path : "Home";
+
+            /** Send the page view with properties */
+            page(pageName);
+            next();
+        });
+    }
+}
+
+/**
+ * Override Meteor.apply (and Meteor.call) with our wrapper.
+ * Our version wraps the user defined callback,
+ * or defines a new one that will track an event if the method did not
+ * throw an error.
+ */
+function setupMethodTracking() {
+    Meteor.connection.apply = _.wrap(Meteor.connection.apply,
+        function(func, name, args, options={}, callback) {
+            if (typeof options === "function") {
+                callback = options;
+                options = {};
+            }
+
+            let track = function(err, res) {
+                if (!err) {
+                    let properties = createProperties({ args, res });
+                    analytics.track(`Called ${name} Method`, properties);
+                }
+            };
+
+            if (callback) {
+                callback = _.wrap(callback, function(originalCallback, err, res) {
+                    track(err, res);
+                    originalCallback(err, res);
+                });
+            } else {
+                callback = track;
+            }
+
+            func = _.bind(func, this);
+            func(name, args, options, callback);
+        }
+    );
+}
+
+/*
+ * When meteor starts, attempt to initialize analytics.js integration
+ * and setup automatic tracking.
+ * @todo Clean this up, use promises or something.
+ * @todo Change to production url.
+ */
+Meteor.startup(() => {
+    let settings = (Meteor.settings || {}).public || {};
+    let appId = (settings.astronomer || {}).appId;
+    let appSecret = (settings.astronomer || {}).appSecret;
+    if (appId && appSecret) {
+        let home = DDP.connect("http://localhost:4000");
+        home.call("/applications/credentials", appId, appSecret, (err, res) => {
+            if (err) {
+                return console.error(err.reason);
+            }
+
+            let params = {
+                appId: appId,
+                identityId: res.credentials.IdentityId,
+                token: res.credentials.Token,
+                identityPoolId: res.identityPoolId,
+                region: "us-east-1"
+            };
+
+            analytics.initialize({ "astronomer": params });
+
+            if (typeof Meteor.user !== "undefined") {
+                setupIdentify();
+            } else {
+                console.warn("Meteor accounts not detected, all events will be anonymous.");
+            }
+            setupRouteTracking();
+            setupMethodTracking();
+        });
+    } else {
+        console.warn("Astronomer keys not found in Meteor.settings, skipping setup.");
+    }
+});
+
