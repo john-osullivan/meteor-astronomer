b567febd77f58cbe98fc3bf3b7171e237837bffa
schnie <greg@differential.com>
2015-06-25 12:00:58 -0400

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "astronomerio:core",
-  version: "0.0.3",
+  version: "0.0.4",
   summary: "Easily push analytics events to astronomer.",
   git: "https://github.com/astronomerio/meteor-astronomer",
   documentation: "README.md"
