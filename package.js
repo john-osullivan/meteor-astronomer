ae4f9df79f70bc27826e096e07cf36453f51e92e
Ry Walker <ry@rywalker.com>
2015-09-20 10:09:17 -0400

    Sync versions

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
     name: "astronomerio:core",
-    version: "0.2.10",
+    version: "0.2.11",
     summary: "Easily push analytics events to astronomer.",
     git: "https://github.com/astronomerio/meteor-astronomer",
     documentation: "README.md"
