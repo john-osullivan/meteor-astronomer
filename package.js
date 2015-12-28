39ef46d38418ea098fe472c9501e10de0e203c91
schnie <greg@differential.com>
2015-12-28 16:01:23 -0500

    add analytics-node

--- package.js
+++ package.js
@@ -15,15 +15,23 @@ Package.onUse(function(api) {
         "underscore",
         "mongo"
     ]);
+
+    Npm.depends({
+        "analytics-node": "https://github.com/astronomerio/analytics-node/archive/af30d8e9b9ec4e96ddb852ab6ba5d3aa8eda61da.tar.gz"
+    });
+
     api.addFiles("dist/snippet.js", "client");
     api.addFiles("dist/tracking.js", "client");
     api.addFiles("dist/browser-policy.js", "server");
     api.addFiles("dist/publications.js", "server");
+    api.addFiles("dist/server.js", "server");
 
     api.use(["accounts-base", "accounts-oauth"], { weak: true });
     api.use("iron:router@1.0.7", "client", { weak: true });
     api.use("meteorhacks:flow-router@1.17.2", "client", { weak: true });
     api.use("browser-policy-content", "server", { weak: true });
+
+    api.export("analytics", "server");
 });
 
 Package.onTest(function(api) {
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const Analytics = Npm.require("analytics-node");
+
+// Grab settings from global object or Meteor.settings.
+const settings = global.AstronomerConfig
+    || (((Meteor.settings || {}).public || {}).astronomer || {});
+
+// Setup dummy methods.
+analytics = {};
+analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "page", "once", "off", "on"];
+for (var t = 0; t < analytics.methods.length; t++) {
+    var e = analytics.methods[t];
+    analytics[e] = function() {};
+}
+
+// Assign real analytics if we have an appId.
+if (settings.appId) {
+    analytics = new Analytics(settings.appId);
+} else {
+    console.warn("Astronomer settings not found in Meteor.settings, skipping setup.");
+}
